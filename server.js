08e4e3bb84fc9f436d3ca0a44b31285c84b05ed3
Abdifatah <abdifatah.101@outlook.com>
2021-10-17 19:03:59 +0100

    Added middleware and port

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 4000;
+
+
+const app = express();
+///middleware
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
