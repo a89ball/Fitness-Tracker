543fa68d8b4641ee333691f81bc5749e0699d1c7
Abdifatah <abdifatah.101@outlook.com>
2021-10-18 01:37:10 +0100

    added schema for workout

--- models/workout.js
+++ models/workout.js
@@ -1,2 +1,41 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
+
+const workoutSchema = new Schema({
+
+    day: {
+        type: Date,
+        default: () => new Date()
+    },
+    exercises: [
+        {
+            type:{
+                type: String,
+                trim: true,
+                required: "Required Field"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Required field"
+            },
+            duration: {
+                type: Number,
+                required: "Required field"
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+        
+            distance: {
+                type: Number
+            }
+        }
+    ]
+},
